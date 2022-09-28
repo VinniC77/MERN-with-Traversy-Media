4d244ab05b8f1fabe3625ff72f5060596eecf48e
viniciusClunc <vinicius@valorizza.com.br>
2022-09-28 14:54:36 -0300

    userController fixed up and readu to authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
-const user = require('../models/userModels')
+const User = require('../models/userModel')
 
 // @desc Register new user
 // @desc POST /api/users
@@ -20,9 +20,27 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('User already exists')
     }
 
+    // Hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
+    // Create the user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword  
+    })
 
-    res.json({message: 'Register user'})
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc Authenticate a user
