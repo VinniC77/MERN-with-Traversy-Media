d56ba3922007786bb875739e0bb965c5ee7fb47e
viniciusClunc <vinicius@valorizza.com.br>
2022-10-04 09:29:34 -0300

    First frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          {/* Precisamos colcoar o Header aqui porque Routes só aceita componentes de rotas */}
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
